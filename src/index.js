77e4bba5be9396b674217938b26eec14f3f5db79
Konekoya <tank158r5155@yahoo.com.tw>
2017-03-22 09:38:12 +0800

    Add youtube search api and fetch data from YT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyDAS05yfxujb-epsgDT6g8y43Wo-Eyedh0';
 
-const API_KEY = 'AIzaSyA5n5y9IVFdWbHzgfUfkclmsvEdy9l3Qxc';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-);
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      console.log(videos);
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
